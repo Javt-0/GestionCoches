696a0ece68544450555117c01f69074f8270d97c
Javt-0 <jonathan96aguilar@gmail.com>
2022-09-28 11:51:16 +0200

    For
    Array de objetos

--- GestionCoches/src/test/GestionCoches.java
+++ GestionCoches/src/test/GestionCoches.java
@@ -13,6 +13,12 @@ public class GestionCoches{
         Vehiculo v1 = new Vehiculo("33M", "BMW", "z1", "Azul", 1.45, true);
         Vehiculo v2 = new Vehiculo();
         Vehiculo v3 = new Vehiculo("22M");
+        
+        Vehiculo v [] = {v1,v2,v3};
+        
+        for(int i=0; i<v.length; i++){
+            System.out.println(v[i]);
+        }
     }
     
 }
