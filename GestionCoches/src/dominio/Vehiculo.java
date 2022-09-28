654950d95417bb72fa82e7f3402aad7718354858
Javt-0 <jonathan96aguilar@gmail.com>
2022-09-28 11:34:10 +0200

    Clases
    Se crea clases en diferente paquete y lo instanciamos

---
+++ GestionCoches/src/dominio/Vehiculo.java
@@ -0,0 +1,75 @@
+
+package dominio;
+
+/**
+ *
+ * @author jonat
+ */
+public class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
+    }
+    
+    
+}
---
+++ GestionCoches/src/test/GestionCoches.java
@@ -0,0 +1,15 @@
+
+package test;
+
+/**
+ *
+ * @author jonat
+ */
+public class GestionCoches {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
